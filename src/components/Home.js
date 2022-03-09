d3ee43c14d37d4be5704999f4c72ba923a2797bc
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2022-03-10 03:14:06 +0530

    Song bar icons added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import BottomNavigation from '@mui/material/BottomNavigation';
 import './Home.css'
+import { PlayCircleFilled, Repeat, Shuffle, SkipNext, SkipPrevious } from '@mui/icons-material';
+import { Grid } from '@mui/material';
 
 function Home() {
   return (
@@ -8,7 +10,39 @@ function Home() {
 
         {/* Bottom player navigatior*/}
         <div>
-            <BottomNavigation className='song_nav_bar'/>
+            <BottomNavigation className='song_nav_bar'>
+                <Grid container>
+                    <Grid item xs={4}>
+
+                    </Grid>
+                    <Grid item xs={4}>
+                        <div className='song_controls'>
+                            <div className='all_icons'>
+                                <div>
+                                    <Shuffle className='song_play song_other'   />
+                                </div>
+                                <div>
+                                    <SkipPrevious className='song_play song_other' />
+                                </div>
+                                <div>
+                                    <PlayCircleFilled className='song_play' fontSize='large'/>
+                                </div>
+                                <div>
+                                    <SkipNext className='song_play song_other'/>
+                                </div>
+                                <div>
+                                    <Repeat className='song_play song_other'/>
+                                </div>
+                            </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+
+                    </Grid>
+                
+                </Grid> 
+                
+            </BottomNavigation>
         </div>
     </div>
   )
