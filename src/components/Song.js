1a0b0028cab76304ada11f9c5d100b5509ea0824
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2022-03-19 02:03:31 +0530

    Add hover over property

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { Avatar, Box, Button, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
+import { Avatar, Box, Button, ButtonBase, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
 
 function Song({songDetails}) {
 
   return (
-            
-              <Card sx={{ height:'fit-content', width:175, flexDirection: 'column' , backgroundColor:'#161616', padding:2, borderRadius:2 }}>
-                    <CardMedia
-                        component="img"
-                        sx={{ minWidth:175, maxWidth:175, minHeight:175 ,maxHeight: 175, borderRadius:2 }}
-                        image={songDetails.songImageUrl}
-                        alt="Live from space album cover"
-                    />
-                    <CardContent sx={{ justifyContent: "center",flexDirection: "column"}}>
-                      <Typography sx={{color:'white', whiteSpace: 'nowrap', overflow: 'hidden',  textOverflow: 'ellipsis', fontWeight:'bold', fontSize:15}} >
-                          {songDetails.songName}
-                      </Typography>
-                    </CardContent>                                                                          
+          
+              <Card sx={{ height:'fit-content', width:175, flexDirection: 'column' , backgroundColor:'#161616'   , '&:hover': {background: "#2c2a35"}, padding:2, borderRadius:2 }}>
+                      <CardMedia
+                          component="img"
+                          sx={{ minWidth:175, maxWidth:175, minHeight:175 ,maxHeight: 175, borderRadius:2 }}
+                          image={songDetails.songImageUrl}
+                          alt="Live from space album cover"
+                      />
+                      <CardContent sx={{ justifyContent: "center",flexDirection: "column"}}>
+                        <Typography sx={{color:'white', whiteSpace: 'nowrap', overflow: 'hidden',  textOverflow: 'ellipsis', fontWeight:'bold', fontSize:15}} >
+                            {songDetails.songName}
+                        </Typography>
+                      </CardContent> 
+                                                                                            
                 </Card>
               
         )
