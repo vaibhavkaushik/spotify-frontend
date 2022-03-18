61cef57436d47e4f3f6b56fd7bc61c6be3f8cf43
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2022-03-19 02:26:46 +0530

    Made song tiles clickable

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Avatar, Box, Button, ButtonBase, Card, CardContent, CardMedia, Grid, Typography } from '@mui/material'
+import { Avatar, Box, Button, ButtonBase, Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material'
 
 function Song({songDetails}) {
 
   return (
-          
-              <Card sx={{ height:'fit-content', width:175, flexDirection: 'column' , backgroundColor:'#161616'   , '&:hover': {background: "#2c2a35"}, padding:2, borderRadius:2 }}>
+              <CardActionArea > 
+              <Card hoverable sx={{ height:'fit-content', width:175, flexDirection: 'column' , backgroundColor:'#161616'   , '&:hover': {background: "#2c2a35"}, padding:2, borderRadius:2 }}>
                       <CardMedia
                           component="img"
                           sx={{ minWidth:175, maxWidth:175, minHeight:175 ,maxHeight: 175, borderRadius:2 }}
@@ -16,10 +16,9 @@ function Song({songDetails}) {
                         <Typography sx={{color:'white', whiteSpace: 'nowrap', overflow: 'hidden',  textOverflow: 'ellipsis', fontWeight:'bold', fontSize:15}} >
                             {songDetails.songName}
                         </Typography>
-                      </CardContent> 
-                                                                                            
+                      </CardContent>                                            
                 </Card>
-              
+                </CardActionArea> 
         )
 }
 
--- src/components/SongTile.js
+++ src/components/SongTile.js
@@ -5,19 +5,19 @@ function SongTile({songDetails}) {
 
   return (
             
-              <Card onClick={() => console.log("CardActionArea clicked")} sx={{ display: 'flex',  backgroundColor:'#2c2a35', '&:hover': {background: "#535353"}, width:'100%' }}>
-                  
+              <Card hoverable onClick={() => console.log("CardActionArea clicked")} sx={{ display: 'flex',  backgroundColor:'#2c2a35', '&:hover': {background: "#535353"}, width:'100%' }}>
+                    <CardActionArea sx={{ display: 'flex'}}>
                       <CardMedia
-                        component="img"
-                        sx={{ minWidth:100, minHeight:100, maxWidth: 100, maxHeight: 100}}
-                        image={songDetails.songImageUrl}
-                    />
-                    <CardContent sx={{display:'flex',justifyContent: "center",flexDirection: "column", width:'100%'}}>
-                      <Typography sx={{width:'100%', whiteSpace: 'nowrap', overflow: 'hidden',  textOverflow: 'ellipsis', color:'white', fontWeight:'bold', fontSize:15}} >
-                          {songDetails.songName}
-                      </Typography>
-                    </CardContent>
-                                                                                            
+                          component="img"
+                          sx={{ minWidth:100, minHeight:100, maxWidth: 100, maxHeight: 100}}
+                          image={songDetails.songImageUrl}
+                      />
+                      <CardContent sx={{display:'flex',justifyContent: "center",flexDirection: "column", width:'100%'}}>
+                        <Typography sx={{width:'100%', whiteSpace: 'nowrap', overflow: 'hidden',  textOverflow: 'ellipsis', color:'white', fontWeight:'bold', fontSize:15}} >
+                            {songDetails.songName}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>                                                        
                 </Card>
         )
 }
