3166063e3ebf5fcc4160a83fa907fa0f93c7f85b
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2022-04-27 23:30:30 +0530

    Sign In/ Sign Up Page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Home from './components/Home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        <Home/>
-      </div>
+        <Route exact path='/' element={<SignUp/>} />        
     </div>
   );
 }
